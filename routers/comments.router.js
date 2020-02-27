5fd4ff84716867257ced92f8a00d9bfcde4de049
conwayhub <ofdermott@gmail.com>
2020-02-27 18:03:53 +0000

    Just slippin' my 405 error handling onto the end of my comments router

--- routers/comments.router.js
+++ routers/comments.router.js
@@ -3,10 +3,12 @@ const {
   patchCommentVotes,
   deleteComments
 } = require("../controllers/comments.controllers.js");
+const { send405Error } = require("../errors");
 
 commentsRouter
   .route("/:comment_id")
   .patch(patchCommentVotes)
-  .delete(deleteComments);
+  .delete(deleteComments)
+  .all(send405Error);
 
 module.exports = { commentsRouter };
