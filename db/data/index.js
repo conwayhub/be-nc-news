8fdc3c7f91dcc9cec4004da870018ce732aa523d
conwayhub <ofdermott@gmail.com>
2020-03-13 21:45:44 +0000

    could it be hosted?

--- db/data/index.js
+++ db/data/index.js
@@ -3,6 +3,6 @@ const ENV = process.env.NODE_ENV || "development";
 const testData = require("./test-data");
 const devData = require("./development-data");
 
-const data = { test: testData, production: devData };
+const data = { test: testData, production: devData, development: devData };
 
 module.exports = data[ENV];
