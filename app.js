645482bdbad7204284bc37cf113b2263a11653bc
conwayhub <ofdermott@gmail.com>
2020-03-16 14:33:58 +0000

    installing cors and adding to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { apiRouter } = require("./routers/api.router");
+const cors = require("cors");
 const {
   customErrorHandler,
   psqlErrorHandler,
@@ -8,6 +9,7 @@ const {
 } = require("./errors/index");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.use(customErrorHandler);
